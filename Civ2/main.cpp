b9bf8d6e47fcc90a41d93539c2ca29090f0255c7
medmaster0 <antbaca9@gmail.com>
2017-06-24 22:46:03 -0600

    Initial Commit

---
+++ Civ2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Civ2
+//
+//  Created by Medz on 6/24/17.
+//  Copyright © 2017 Medz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
